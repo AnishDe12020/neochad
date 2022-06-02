ed1a1014dd98758ccac1d5e57fb9eeeae9493c5f
AnishDe12020 <63192115+AnishDe12020@users.noreply.github.com>
2022-06-02 15:58:41 +0530

    fix(nvim-lsp-installer): put in the right config

--- lua/plugins/configs/lsp_installer.lua
+++ lua/plugins/configs/lsp_installer.lua
@@ -1,78 +1,36 @@
-local present, lspconfig = pcall(require, "lspconfig")
+local present, lsp_installer = pcall(require, "nvim-lsp-installer")
 
 if not present then
    return
 end
 
-local M = {}
-local utils = require "core.utils"
+local options = {
+   -- ensure_installed is not needed as automatic_installation is enabled
+   -- then any lsp server you setup by lspconfig is going to get installed automatically!
 
-require("plugins.configs.others").lsp_handlers()
+   -- ensure_installed = { "lua" },
+   automatic_installation = true,
 
--- Borders for LspInfo winodw
-local win = require "lspconfig.ui.windows"
-local _default_opts = win.default_opts
-
-win.default_opts = function(options)
-   local opts = _default_opts(options)
-   opts.border = "single"
-   return opts
-end
-
-M.on_attach = function(client, bufnr)
-   client.resolved_capabilities.document_formatting = false
-   client.resolved_capabilities.document_range_formatting = false
-
-   local lsp_mappings = utils.load_config().mappings.lspconfig
-   utils.load_mappings({ lsp_mappings }, { buffer = bufnr })
-end
-
-local capabilities = vim.lsp.protocol.make_client_capabilities()
-
-capabilities.textDocument.completion.completionItem = {
-   documentationFormat = { "markdown", "plaintext" },
-   snippetSupport = true,
-   preselectSupport = true,
-   insertReplaceSupport = true,
-   labelDetailsSupport = true,
-   deprecatedSupport = true,
-   commitCharactersSupport = true,
-   tagSupport = { valueSet = { 1 } },
-   resolveSupport = {
-      properties = {
-         "documentation",
-         "detail",
-         "additionalTextEdits",
+   ui = {
+      icons = {
+         server_installed = " ",
+         server_pending = " ",
+         server_uninstalled = " ﮊ",
+      },
+      keymaps = {
+         toggle_server_expand = "<CR>",
+         install_server = "i",
+         update_server = "u",
+         check_server_version = "c",
+         update_all_servers = "U",
+         check_outdated_servers = "C",
+         uninstall_server = "X",
       },
    },
+
+   max_concurrent_installers = 20,
 }
 
-lspconfig.sumneko_lua.setup {
-   on_attach = M.on_attach,
-   capabilities = capabilities,
+options = require("core.utils").load_override(options, "williamboman/nvim-lsp-installer")
 
-   settings = {
-      Lua = {
-         diagnostics = {
-            globals = { "vim", "nvchad" },
-         },
-         workspace = {
-            library = {
-               [vim.fn.expand "$VIMRUNTIME/lua"] = true,
-               [vim.fn.expand "$VIMRUNTIME/lua/vim/lsp"] = true,
-            },
-            maxPreload = 100000,
-            preloadFileSize = 10000,
-         },
-      },
-   },
-}
-
--- requires a file containing user's lspconfigs
-local addlsp_confs = utils.load_config().plugins.options.lspconfig.setup_lspconf
-
-if #addlsp_confs ~= 0 then
-   require(addlsp_confs).setup_lsp(M.on_attach, capabilities)
-end
-
-return M
+lsp_installer.setup(options)
