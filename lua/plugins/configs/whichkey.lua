32cdd5db283f9ebeb64d802a7f2fb5b489727b8b
AnishDe12020 <63192115+AnishDe12020@users.noreply.github.com>
2022-06-06 16:12:25 +0530

    fix(lspconfig,whichkkey): get back the lsp mappings (idk some fix i copied from nvchad repo)

--- lua/plugins/configs/whichkey.lua
+++ lua/plugins/configs/whichkey.lua
@@ -6,6 +6,27 @@ end
 
 local options = {
 
+   -- NOTE : this mode_opts table isnt in the default whichkey config
+   --  Its added here so you could configure it in chadrc
+
+   mode_opts = {
+      n = {
+         mode = "n",
+      },
+
+      v = {
+         mode = "v",
+      },
+
+      i = {
+         mode = "i",
+      },
+
+      t = {
+         mode = "t",
+      },
+   },
+
    icons = {
       breadcrumb = "»", -- symbol used in the command line area that shows your active key combo
       separator = "  ", -- symbol used between a key and it's label
@@ -36,14 +57,27 @@ local options = {
 
 options = require("core.utils").load_override(options, "folke/which-key.nvim")
 
-local utils = require "core.utils"
-
-local mappings = utils.load_config().mappings
+local mappings = require("core.utils").load_config().mappings
 local mapping_groups = { groups = vim.deepcopy(mappings.groups) }
 
 mappings.disabled = nil
 mappings.groups = nil
 
-utils.load_mappings(mapping_groups)
+-- register mappings
+local function register_mappings(maps, opts)
+   for mode, opt in pairs(opts.mode_opts) do
+      for key, value in pairs(maps) do
+         if key ~= "lspconfig" then
+            if value[mode] then
+               local mode_opts = value["mode_opts"] and vim.tbl_deep_extend("force", opt, value["mode_opts"]) or opt
+               wk.register(value[mode], mode_opts)
+            end
+         end
+      end
+   end
+end
+
+register_mappings(mappings, options)
+register_mappings(mapping_groups, options)
 
 wk.setup(options)
